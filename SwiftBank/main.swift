ce6b90e80fe22159f7676fad8752ec00a71054b0
Keval <keval.langalia@live.com>
2021-02-26 09:28:04 +0530

    FileUtils added and file and json related files moved to fileUtils

--- SwiftBank/main.swift
+++ SwiftBank/main.swift
@@ -8,78 +8,7 @@
 import Foundation
 
 
-extension URL {
-    static func createFolder(folderName: String) -> URL? {
-        let fileManager = FileManager.default
-        // Get document directory for device, this should succeed
-        if let documentDirectory = fileManager.urls(for: .documentDirectory,
-                                                    in: .userDomainMask).first {
-            // Construct a URL with desired folder name
-            let folderURL = documentDirectory.appendingPathComponent(folderName)
-            // If folder URL does not exist, create it
-            if !fileManager.fileExists(atPath: folderURL.path) {
-                do {
-                    // Attempt to create folder
-                    try fileManager.createDirectory(atPath: folderURL.path,
-                                                    withIntermediateDirectories: true,
-                                                    attributes: nil)
-                    // try fileManager.createFile(atPath: folderURL.path, contents: nil)
-                } catch {
-                    // Creation failed. Print error & return nil
-                    print(error.localizedDescription)
-                    return nil
-                }
-            }
-            // Folder either exists, or was created. Return URL
-            return folderURL
-        }
-        // Will only be called if document directory not found
-        return nil
-    }
-}
 
-// converting object to string
-func getJsonString(of obj: Customer) -> String{
-    do{
-        let jsonEncoder = JSONEncoder()
-        let jsonData = try jsonEncoder.encode(obj)
-        if let json = String(data: jsonData, encoding: String.Encoding.utf8){
-            print(json)
-            return json
-        }
-    }catch{ }
-    return ""
-}
-
-func saveJsonFile(of data: String) {
-    if let documentDirectory = FileManager.default.urls(for: .documentDirectory,
-                                                        in: .userDomainMask).first
-    {
-        let pathWithFilename = documentDirectory.appendingPathComponent("myJsonString.json")
-        print(pathWithFilename) // this is thet path where our file will be stored
-        do {
-            try data.write(to: pathWithFilename, atomically: true, encoding: .utf8)
-        } catch {
-            // Handle error
-        }
-    }
-}
-
-func read() -> Customer? {
-    if let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
-        let url = documentDirectory.appendingPathComponent("BankData.json")
-        let data = NSData(contentsOf: url)
-        
-        do {
-            // converting data to object(i.e Product in our case)
-            if let payload = data as Data? {
-                let product = try JSONDecoder().decode(Customer.self, from: payload)
-                return product
-            }
-        } catch {}
-    }
-    return nil
-}
 
 
 
@@ -125,6 +54,5 @@ if let path = URL.createFolder(folderName: "SwiftBank") {
     }
     
 }
-
 
 
