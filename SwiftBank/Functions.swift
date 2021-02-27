7cf2f31513a23cfa99af201f03feba9bdebe767f
Keval <keval.langalia@live.com>
2021-02-27 09:05:23 +0530

    making modular

--- SwiftBank/Functions.swift
+++ SwiftBank/Functions.swift
@@ -29,6 +29,44 @@ func registerUser() -> CustomerDetails {
     return CustomerDetails(name: name, contactNo: contactNo, address: address, password: pass)
 }
 
+func letAddBankAccounts() -> Accounts {
+    
+    let bankAccounts = Accounts()
+    
+    repeat{
+        
+        print("Which account would you like to open?\n1 - Salary account\n2 - Saving account\n3 - Fixed Deposit account")
+        let choice = Int(readLine()!)!
+        
+        switch choice {
+            case 1:
+                // open salary account
+                let salAcc = createSalaryAcc()
+                bankAccounts.salaryAcc = salAcc
+                
+            case 2:
+                // open saving account
+                let savAcc = createSavingAcc()
+                bankAccounts.SavingsAcc = savAcc
+        
+            case 3:
+                // open fixed deposit account
+                let fdAcc = createFdAcc()
+                bankAccounts.FixedDepositAcc = fdAcc
+        
+            default:
+                // wrong choice
+                print("Sorry, incorrect input.")
+        }
+        
+        print("Would you like to add more bank account? y/n")
+        
+    } while(readLine()! == "y")
+    
+    return bankAccounts
+    
+}
+
 func generateNextAccountNumber() -> String {
     var accNo = 0
     var lastAccNo = 0
--- SwiftBank/main.swift
+++ SwiftBank/main.swift
@@ -12,42 +12,12 @@ var customers: Customers?
 var savedData = getSavedData()
 if savedData.isFirstTime {
     // ask user to enter new customer
-    var custDetail = registerUser()
+    let custDetail = registerUser()
     
     print("Thanks for registering.")
     
-    let bankAccounts = Accounts()
-    repeat{
-        
-        print("Which account would you like to open?\n1 - Salary account\n2 - Saving account\n3 - Fixed Deposit account")
-        let choice = Int(readLine()!)!
-        
-        switch choice {
-            case 1:
-                // open salary account
-                let salAcc = createSalaryAcc()
-                bankAccounts.salaryAcc = salAcc
-                
-            case 2:
-                // open saving account
-                let savAcc = createSavingAcc()
-                bankAccounts.SavingsAcc = savAcc
-        
-            case 3:
-                // open fixed deposit account
-                let fdAcc = createFdAcc()
-                bankAccounts.FixedDepositAcc = fdAcc
-        
-            default:
-                // wrong choice
-                print("Sorry, incorrect input.")
-        }
-        
-        print("Would you like to add more bank account? y/n")
-        
-    } while(readLine()! == "y")
+    custDetail.addBankAccounts(accs: letAddBankAccounts())
     
-    custDetail.accounts = bankAccounts
     savedData.cust = Customers(custs: [custDetail])
     var jsonStr = ""
     if let data = savedData.cust {
@@ -55,7 +25,6 @@ if savedData.isFirstTime {
     }
     
     saveJsonFile(of: jsonStr)
-    
     
 }
 
